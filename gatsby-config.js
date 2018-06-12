9bc59a23b59ef8769c0fac30e653661d7e122ac0
lutangar <johan.dufour@gmail.com>
2018-06-12 09:57:08 +0200

    add gatsby conf for the doc

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Cryptocurrencies core wallets configuration map in json',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+  pathPrefix: `/wallets`,
+};
